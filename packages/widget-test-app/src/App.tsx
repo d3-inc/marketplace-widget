29f6a0a4047a9ccddc9a2e08ad4a15b72e04f2e3
junaid-faryad <junaid.faryad@gmail.com>
2024-10-25 07:53:35 +0500

    fix: update deploy

--- packages/widget-test-app/src/App.tsx
+++ packages/widget-test-app/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={'https://d3.app/favicon.png'} className="logo" alt="Vite logo" />
         </a>
         <h2 style={{ fontWeight: 500 }}>D3 Marketplace Widget</h2>
-        <p>D3 marketplace widget playground</p>
+        <p>Widget playground</p>
       </div>
       <D3Widget
         appName="Widget Test app"
