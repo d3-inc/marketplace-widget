258193989ca4ab85c5c587d49a5e287daadf51d0
s2dia <sadia.sumera007@gmail.com>
2025-03-09 01:11:39 +0500

    feat: jest and react testing library for testing

---
+++ packages/marketplace-widget/jest.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': ['ts-jest', { useESM: true }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
